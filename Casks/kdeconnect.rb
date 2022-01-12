5b80c63709ed1d5949390d9e3f22b10802f87885
hybras <24651269+hybras@users.noreply.github.com>
2022-01-12 15:02:26 -0500

    Use match instead of scan

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -2,7 +2,7 @@ cask "kdeconnect" do
   version "21.12.1-818"
   sha256 "3bbc496ee2d93f126ed3854458d361e0214b3099f1a7f9b3658e8906a2b2968d"
 
-  url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/#{version.scan(/\d+(?:[.-](\d+))+/)[0][0]}/artifact/kdeconnect-kde-#{version}-macos-64-clang.dmg"
+  url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/#{version.match(/\d+(?:[.-](\d+))+/)[1]}/artifact/kdeconnect-kde-#{version}-macos-64-clang.dmg"
   name "KDE Connect"
   desc "Enabling communication between all your devices"
   homepage "https://kdeconnect.kde.org/"

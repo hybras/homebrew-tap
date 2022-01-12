5afc449fd6fd7f7bddf12e9e124c593aeb8d4c4d
hybras <24651269+hybras@users.noreply.github.com>
2022-01-12 15:08:16 -0500

    Update createzap to v0.2

--- Formula/homebrew-createzap.rb
+++ Formula/homebrew-createzap.rb
@@ -1,9 +1,9 @@
 class HomebrewCreatezap < Formula
   desc "Homebrew external command that generates zap stanzaz for casks"
   homepage "https://github.com/hybras/homebrew-createzap"
-  url "https://github.com/hybras/homebrew-createzap/archive/v0.1.0.tar.gz",
+  url "https://github.com/hybras/homebrew-createzap/archive/v0.2.0.tar.gz",
       verified: "github.com/hybras/homebrew-createzap"
-  sha256 "c0f907f775418e4f35cc8502366bc0ad1f87b3e3ac366b350ea03dd27fc3a6c2"
+  sha256 "ea7c8dd6a212ee7c1f074c689df29890399c388c22ea0c622857477200130ed1"
   license "MIT"
 
   depends_on "fd"

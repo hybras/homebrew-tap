9dab07cc490d9be86fb1fd06beb6c250df75847e
hybras <24651269+hybras@users.noreply.github.com>
2022-01-20 15:24:12 -0500

    Format pijul formula code

--- Formula/pijul.rb
+++ Formula/pijul.rb
@@ -1,14 +1,14 @@
 class Pijul < Formula
-  desc "A sound and fast distributed version control system"
+  desc "Sound and fast distributed version control system"
   homepage "https://pijul.com"
-  license "GPL-2.0-or-later"
   url "https://static.crates.io/crates/pijul/pijul-1.0.0-alpha.57.crate"
   sha256 "d6dd02c47ffb436f8645939d8e172bd07d7f908cf7ea49f2aba6e23f1e58821a"
+  license "GPL-2.0-or-later"
 
-  depends_on "rust" => :build
-  depends_on "llvm" => :build
   depends_on "libsodium" => :build
+  depends_on "llvm" => :build
   depends_on "openssl" => :build
+  depends_on "rust" => :build
   depends_on "xxhash" => :build
   depends_on "zstd" => :build
 

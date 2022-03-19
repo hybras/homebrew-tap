6273c9e4978b69e3e02dfaeebc6b4b2f40925c8f
hybras <24651269+hybras@users.noreply.github.com>
2022-03-19 14:42:54 -0400

    Update kdeconnect from 21.12.2-845 to 21.12.3-887

--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -1,6 +1,6 @@
 cask "kdeconnect" do
-  version "21.12.2-845"
-  sha256 "748385741bcd30e7bff1b8a1b1584fdef70d1dfcd7b120862bcc6b90cc218e34"
+  version "21.12.3-887"
+  sha256 "97b31a1abed9a6d12f886b36f8cc9aa9276ee3f99e06e232ea9f5c88d2205cce"
 
   url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/#{version.match(/\d+(?:[.-](\d+))+/)[1]}/artifact/kdeconnect-kde-#{version}-macos-64-clang.dmg"
   name "KDE Connect"

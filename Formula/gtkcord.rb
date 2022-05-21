9caa77f83685432293175cb6c3355c14d9f62f60
hybras <24651269+hybras@users.noreply.github.com>
2022-05-21 18:09:30 -0400

    Gtkcord

---
+++ Formula/gtkcord.rb
@@ -0,0 +1,20 @@
+class Gtkcord < Formula
+  desc "Gtk3 Discord client"
+  homepage "https://github.com/diamondburned/gtkcord4"
+  url "https://github.com/diamondburned/gtkcord4/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "bca33768dfd7695cfc8e16e6d0ff0f627b23ae47065cf04e461ef41fcdcf30f5"
+  license "GPLv3"
+
+  depends_on "go" => :build
+  depends_on "gtk4" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "libcanberra" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "#{bin}"/"gtkcord", "--help"
+  end
+end

3b323e2a3b1a017c29bec034f7245677216a496f
hybras <24651269+hybras@users.noreply.github.com>
2022-05-23 21:56:21 -0400

    Set version latest in kde connect casks
    Instead of manually upgrading the package version on every release,
    package will check for latest version on download

--- Casks/kdeconnect-nightly.rb
+++ Casks/kdeconnect-nightly.rb
@@ -1,8 +1,8 @@
 cask "kdeconnect-nightly" do
-  version "1533"
-  sha256 "e5fda41de97784d9602cfb35321a275f77bcc4043ff0e33ad6a2fe1ae2fa020b"
-
-  url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Nightly_macos/#{version}/artifact/kdeconnect-kde-master-#{version}-macos-64-clang.dmg"
+  version :latest
+  sha256 :no_check
+ 
+  url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Nightly_macos/lastStableBuild/artifact/kdeconnect-kde-master-1535-macos-64-clang.dmg"
   name "KDE Connect"
   desc "Enabling communication between all your devices"
   homepage "https://kdeconnect.kde.org/"
--- Casks/kdeconnect.rb
+++ Casks/kdeconnect.rb
@@ -1,14 +1,14 @@
 cask "kdeconnect" do
-  version "22.04.1-950"
-  sha256 "9e9387fd03171a32a6f632967fb2fc47960b0b7b187f60bec7f6c3f1d8d65516"
+  version :latest
+  sha256 :no_check
 
-  url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/#{version.match(/\d+(?:[.-](\d+))+/)[1]}/artifact/kdeconnect-kde-#{version}-macos-64-clang.dmg"
+  url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Release_macos/lastStableBuild/artifact/kdeconnect-kde-22.04.1-952-macos-64-clang.dmg"
   name "KDE Connect"
   desc "Enabling communication between all your devices"
   homepage "https://kdeconnect.kde.org/"
 
   livecheck do
-    url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/lastSuccessfulBuild/artifact/"
+    url "https://binary-factory.kde.org/job/kdeconnect-kde_Release_macos/lastStableBuild/artifact/"
     regex(/href=.*?kdeconnect-kde[._-]v?(\d+(?:[.-]\d+)+)-macos-64-clang\.dmg/i)
   end
 

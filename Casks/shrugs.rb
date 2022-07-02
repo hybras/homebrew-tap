e2cce82bf5357a263be933d9c9d1af3c8bc24b75
hybras <24651269+hybras@users.noreply.github.com>
2022-07-02 19:52:35 -0400

    Cask shrugs

---
+++ Casks/shrugs.rb
@@ -0,0 +1,24 @@
+cask "shrugs" do
+  # This cask obeys semver. The 4th number (the one oafter the v) is from sparkle and incremented on new versions / releases.
+  version "1.2.2-v74"
+  sha256 "7d175210fb33681ee947a5556fe0b2b59b23af9043b5e74bb0e194621f59ea75"
+
+  url "https://shrugs.app/downloads/Shrugs.app-#{version}.zip"
+  name "Shrugs"
+  desc "Native Slack Client"
+  homepage "https://shrugs.app/"
+
+  livecheck do
+    url "https://shrugs.app/downloads/appcast.xml"
+    strategy :sparkle do |item|
+      "#{item.short_version}-v#{item.version}"
+    end
+  end
+
+  app "Shrugs.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/de.zeezide.swift.see1.shrugs/",
+    "~/Library/Containers/de.zeezide.swift.see1.shrugs/",
+  ]
+end

27fbd3a1ed21a33b1e05f32f9bf1ac079707ebee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-07-07 05:52:27 +0000

    Update webcord from 3.3.0 to 3.4.0

--- Casks/webcord.rb
+++ Casks/webcord.rb
@@ -1,15 +1,15 @@
 cask "webcord" do
-  version "3.3.0"
+  version "3.4.0"
 
-  if Hardware::CPU.intel? then
-    sha256 "33e0e294fc788ac7b98ffb4294b8f851f9c540943c1913f516506ff3def967cc"
+  if Hardware::CPU.intel?
+    sha256 "d8277caccbdc97e473142e1183b26eb97e380600164747b4babf3c6f376f67d7"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-x64.dmg"
   else
-    sha256 "668d167819b69bffd16d6c0ffb94fd08e9b3baf64c4dcec4715a795299a0bf2f"
+    sha256 "d3b40853bf1efd0a1597ac5612a600e145c962070ff5804a7e9e1fb75a414cc3"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-arm64.dmg"
   end
   name "WebCord"
-  desc "A Discord and Fosscord web-based client made with the :electron:."
+  desc "Discord and Fosscord web-based client made with the :electron:"
   homepage "https://github.com/SpacingBat3/WebCord"
 
   app "WebCord.app"

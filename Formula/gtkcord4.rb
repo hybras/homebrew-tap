8c2087e086b7e7d2a72e5b88f4ae408d1c71bac1
hybras <24651269+hybras@users.noreply.github.com>
2022-07-17 10:58:21 -0400

    Brew audit gtkcord4
    * Fix license identifier
    * use to_s instead of string interpolation
    * reorder deps

--- Formula/gtkcord4.rb
+++ Formula/gtkcord4.rb
@@ -3,11 +3,11 @@ class Gtkcord4 < Formula
   homepage "https://github.com/diamondburned/gtkcord4"
   url "https://github.com/diamondburned/gtkcord4/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "bca33768dfd7695cfc8e16e6d0ff0f627b23ae47065cf04e461ef41fcdcf30f5"
-  license "GPLv3"
+  license "GPL-3.0-or-later"
 
   depends_on "go" => :build
-  depends_on "gtk4" => :build
   depends_on "gobject-introspection" => :build
+  depends_on "gtk4" => :build
   depends_on "libcanberra" => :build
 
   def install
@@ -15,6 +15,6 @@ class Gtkcord4 < Formula
   end
 
   test do
-    system "#{bin}"/"gtkcord", "--help"
+    system bin.to_s/"gtkcord", "--help"
   end
 end

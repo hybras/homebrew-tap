52fcff1f527c21c55957138d504717b6e5ac02bf
hybras <24651269+hybras@users.noreply.github.com>
2022-07-25 09:33:49 -0400

    Update webcord from 3.5.1 to 3.5.2

--- Casks/webcord.rb
+++ Casks/webcord.rb
@@ -1,11 +1,11 @@
 cask "webcord" do
-  version "3.5.1"
+  version "3.5.2"
 
   if Hardware::CPU.intel?
-    sha256 "350492f4ef13627b869e613c327695c1f9234a07a66540fbd168c9a13a77df9a"
+    sha256 "4aefd56c91850583349377d7efdea2fbec66b8fbf30607580c3243a0444d56e8"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-x64.dmg"
   else
-    sha256 "89f769e1faf464b01f60520f3b304e5e9c342c114de5d79f0da890d08c766ef5"
+    sha256 "478b8074f1a8ca39a1b7bd6a3f0a577c7e88396bc5a942488defc6219bd7f6e7"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-arm64.dmg"
   end
   name "WebCord"

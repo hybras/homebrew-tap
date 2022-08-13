1abb4e025c78630fc6f7da0588b47e89327dd24a
Hybras <24651269+hybras@users.noreply.github.com>
2022-08-13 10:09:47 -0400

    Update webcord from 3.6.0 to 3.7.1 (#40)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/webcord.rb
+++ Casks/webcord.rb
@@ -1,11 +1,11 @@
 cask "webcord" do
-  version "3.6.0"
+  version "3.7.1"
 
   if Hardware::CPU.intel?
-    sha256 "fdf783f135fa6ac4ad6b91f737ceadd3685f33e9e7e2398af897858f1e05b4c2"
+    sha256 "50ad50c626ced7d9cc2ccf214691a953fc23856515ff40f5288eeb86eac81781"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-x64.dmg"
   else
-    sha256 "1ea32f44e6eb9fbbaf24a6e5674820b9b5794c0bd05784c4a74937f48f997a56"
+    sha256 "ff018566f8a5410c1b78e77ae015f77644ee866062c5079ab6440ffa03b35547"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-arm64.dmg"
   end
   name "WebCord"

eb4abc5b026fe04dbd8bcaee2b41e1c9a9228745
Hybras <24651269+hybras@users.noreply.github.com>
2022-09-08 17:30:25 -0400

    czkawka 5.0.2 (#46)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/czkawka.rb
+++ Formula/czkawka.rb
@@ -1,8 +1,8 @@
 class Czkawka < Formula
   desc "Find duplicates, empty folders, similar images etc."
   homepage "https://github.com/qarmin/czkawka"
-  url "https://github.com/qarmin/czkawka/archive/refs/tags/5.0.1.tar.gz"
-  sha256 "d06730332adba52d343fa46191fc370b41a9a355ad0eeb354dd872773bb0cd20"
+  url "https://github.com/qarmin/czkawka/archive/refs/tags/5.0.2.tar.gz"
+  sha256 "5c3b0a3017804037d742a18c302b405206def6d569e752c7ebc2adb3ccc45421"
   license "MIT"
 
   depends_on "rust" => :build

f2d76fbf610cb0be83ac74b52e9f8a7e59041344
hybras <24651269+hybras@users.noreply.github.com>
2022-09-17 11:53:34 -0400

    Use cider dmg from github releases

--- Casks/cider.rb
+++ Casks/cider.rb
@@ -1,8 +1,8 @@
 cask "cider" do
-  version :latest
-  sha256 :no_check
+  version "1.5.6"
+  sha256 "4dadcda0a36d7712c13a7c9430fd814037d34b35c38760d01920299366885047"
 
-  url "https://github.com/ciderapp/Cider/releases/download/macos-beta/Cider.dmg",
+  url "https://github.com/ciderapp/cider-releases/releases/download/v#{version}/Cider-#{version}-universal.dmg",
     verified: "github.com/ciderapp"
   name "Cider"
   desc "Apple Music Client"

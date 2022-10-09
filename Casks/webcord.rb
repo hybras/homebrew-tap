861dd911aa287e8e56985b7c4d64caf0c8a86607
Hybras <24651269+hybras@users.noreply.github.com>
2022-10-09 12:43:41 -0400

    Update webcord from 3.8.3 to 3.8.6 (#56)

--- Casks/webcord.rb
+++ Casks/webcord.rb
@@ -1,12 +1,12 @@
 cask "webcord" do
-  version "3.8.3"
+  version "3.8.6"
 
   on_intel do
-    sha256 "e8a44949daf4b6426a0530daccabd80af0d8c6489e3893e1302205d00066e4e8"
+    sha256 "e9faab79e0313bf6fa4e5a3c3f367cb5d19fe2698a810d51e8b7823db82616c2"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-x64.dmg"
   end
   on_arm do
-    sha256 "7bc343562f165d05f3125e51a87b971a144bd7c6733b51f07685992b91fca3a8"
+    sha256 "54e3bba856af39e9f1ae94b5d86f84658b42a7aa7ad7fd3beee69f73f04e30e4"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-arm64.dmg"
   end
 

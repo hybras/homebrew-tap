e5ad8d4a609e917c61339a6b87f1a5029f261503
Hybras <24651269+hybras@users.noreply.github.com>
2022-10-22 12:45:36 -0400

    Update kdeconnect-nightly from 1677 to 1684 (#63)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/kdeconnect-nightly.rb
+++ Casks/kdeconnect-nightly.rb
@@ -1,6 +1,6 @@
 cask "kdeconnect-nightly" do
-  version "1677"
-  sha256 "96c1f063f8976100dda6199a018c7eb35dad63398b0571e3ffd3e870db6969f1"
+  version "1684"
+  sha256 "8c47ab7713be7ccb2d0263cc3b0974c3bb7105464c42a192ca6635831ff99d06"
 
   url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Nightly_macos/#{version}/artifact/kdeconnect-kde-master-#{version}-macos-64-clang.dmg"
   name "KDE Connect"

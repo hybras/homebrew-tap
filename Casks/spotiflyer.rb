7fafe52b39dee2c646321f0a9f38bf74904668ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2022-10-27 05:55:24 +0000

    Update spotiflyer from 3.6.2 to 3.6.3

--- Casks/spotiflyer.rb
+++ Casks/spotiflyer.rb
@@ -1,6 +1,6 @@
 cask "spotiflyer" do
-  version "3.6.2"
-  sha256 "00dea19170dd6896bdb20ae05e18aeaca822ea8392a3af399afa43913df3406b"
+  version "3.6.3"
+  sha256 "787970c64acd30865d1e8649fe404dc5841d7d87910f1d42d1862fd21703d2dd"
 
   url "https://github.com/Shabinder/SpotiFlyer/releases/download/v#{version}/SpotiFlyer-#{version}.dmg"
   name "SpotiFlyer"

0377620e760db16ad2a1b44b14fac0cc29f5f410
hybras <24651269+hybras@users.noreply.github.com>
2022-11-27 09:02:20 -0500

    Add transitive dependencies to pkgconfig path

--- Formula/gtkcord4.rb
+++ Formula/gtkcord4.rb
@@ -7,10 +7,42 @@ class Gtkcord4 < Formula
 
   depends_on "go" => :build
   depends_on "gobject-introspection" => :build
+  depends_on "pkg-config" => :build
   depends_on "gtk4" => :build
   depends_on "libcanberra"
 
   def install
+    deps = %w[
+      glib
+      pcre2
+      pango
+      gdk-pixbuf
+      graphene
+      harfbuzz
+      libpng
+      libjpeg
+      freetype
+      cairo
+      pixman
+      fontconfig
+      graphite2
+      libxcb
+      fribidi
+      libtiff
+      libxau
+      libpthread-stubs
+      libxdmcp
+      libxrender
+      libx11
+      libxext
+    ]
+
+    for dep in deps do
+      ENV.prepend_path "PKG_CONFIG_PATH", Formula[dep].opt_lib/"pkgconfig"
+    end
+
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["xorgproto"].opt_share/"pkgconfig"
+
     system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 

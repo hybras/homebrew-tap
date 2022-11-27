aee5d53697af38b3d4760c19560afcf93e1b5714
hybras <24651269+hybras@users.noreply.github.com>
2022-11-27 09:09:09 -0500

    Update livecheck regex

--- Casks/kdeconnect-nightly.rb
+++ Casks/kdeconnect-nightly.rb
@@ -2,14 +2,14 @@ cask "kdeconnect-nightly" do
   version "1691"
   sha256 "8965022cc9066db0e0b67fba72b638dae2a3c2bdc528d5b8dd60f2bbafae8756"
 
-  url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Nightly_macos/#{version}/artifact/kdeconnect-kde-master-#{version}-macos-64-clang.dmg"
+  url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Nightly_macos/#{version}/artifact/kdeconnect-kde-master-#{version}-macos-clang-x86_64.dmg"
   name "KDE Connect"
   desc "Enabling communication between all your devices"
   homepage "https://kdeconnect.kde.org/"
 
   livecheck do
     url "https://binary-factory.kde.org/job/kdeconnect-kde_Nightly_macos/lastStableBuild/artifact/"
-    regex(/href=.*?kdeconnect-kde-master-(\d+)-macos-64-clang\.dmg/i)
+    regex(/href=.*?kdeconnect-kde-master-(\d+)-macos-clang-x86_64\.dmg/i)
   end
 
   depends_on formula: "dbus"

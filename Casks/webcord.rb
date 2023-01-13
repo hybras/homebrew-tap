c671d768289b1506441f52e6b34acd97f1f40892
Hybras <24651269+hybras@users.noreply.github.com>
2023-01-13 13:29:08 -0500

    Update webcord from 3.10.0 to 4.1.1 (#78)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/webcord.rb
+++ Casks/webcord.rb
@@ -1,12 +1,12 @@
 cask "webcord" do
-  version "3.10.0"
+  version "4.1.1"
 
   on_intel do
-    sha256 "017acd7602943620178319c2a264d9009969a9aa439a7bf1132bc375e022ee70"
+    sha256 "a95addc83a9c0add447600266bc946c58b61095d1626c2fc3b0f2cd0202a0f8c"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-x64.dmg"
   end
   on_arm do
-    sha256 "928d880c119be9313664944f8d038863f41e47a770c6d399efa427352f9d1acb"
+    sha256 "d8d69dde08175d1269e2e07ee8acee988638fcd5c56bcf57ec038c2dd7821979"
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-arm64.dmg"
   end
 

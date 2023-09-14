1d4a4308dc9ac8fcc3390a5a34440eb459d1d09e
Hybras <24651269+hybras@users.noreply.github.com>
2023-09-14 13:28:25 +0000

    kdeconnect-nightly 2008

--- Casks/kdeconnect-nightly.rb
+++ Casks/kdeconnect-nightly.rb
@@ -1,6 +1,6 @@
 cask "kdeconnect-nightly" do
-  version "1826"
-  sha256 "2a132eea67b7cdaa5064119c4e5e255424099c3143fdb56ce89d8081a8baa57e"
+  version "2008"
+  sha256 "d6ab2a32b9c3c6a87161a477d324b635ff15a66ff86e1ad8d375d97f75128978"
 
   url "https://binary-factory.kde.org/view/MacOS/job/kdeconnect-kde_Nightly_macos/#{version}/artifact/kdeconnect-kde-master-#{version}-macos-clang-x86_64.dmg"
   name "KDE Connect"

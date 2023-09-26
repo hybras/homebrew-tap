be7e0b0c44853633c402eba31b992034417a9695
hybras <24651269+hybras@users.noreply.github.com>
2023-09-25 21:36:13 -0400

    Bump displaylink to 1.9

--- Casks/displaylink-manager.rb
+++ Casks/displaylink-manager.rb
@@ -1,13 +1,18 @@
 cask "displaylink-manager" do
-  version "1.7.1"
-  sha256 "32e2c0dce4888895e59e50181f0e15118d8f066abb4568e994878ce0294d422c"
+  version "1.9"
+  sha256 "cd7f7c7c313b0699bfa187f7112a45e5c5441264447b381569839318676208aa"
 
-  url "https://www.synaptics.com/sites/default/files/exe_files/2022-07/DisplayLink%20Manager%20Graphics%20Connectivity#{version}-EXE.pkg"
+  url "https://www.synaptics.com/sites/default/files/exe_files/2023-07/DisplayLink%20Manager%20Graphics%20Connectivity#{version}-EXE.pkg"
   name "DisplayLink Manager"
-  desc ""
+  desc "Drivers for DisplayLink dock, adapter or monitor"
   homepage "https://www.synaptics.com/products/displaylink-graphics/downloads/macos"
 
   pkg "DisplayLink Manager Graphics Connectivity#{version}-EXE.pkg"
+
+  livecheck do
+    # TODO
+    skip "No version information available"
+  end
 
   uninstall pkgutil: "com.displaylink.displaylinkmanagerapp"
 end

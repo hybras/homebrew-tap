51a37e3b592c4dbac228e3c294ff9dc6a9f5eae7
hybras <24651269+hybras@users.noreply.github.com>
2023-09-25 21:16:09 -0400

    Bump arna

--- Casks/arna.rb
+++ Casks/arna.rb
@@ -1,6 +1,6 @@
 cask "arna" do
-  version "2.0.5"
-  sha256 "7b87e27078bdeee11704a89fb52bb7eed056128b26e0a95a23678e208943983a"
+  version "2.1.0"
+  sha256 "81c7d29f4ad99f34062d028cf22e828314ac371aa594b36ba9bef3db5243d580"
 
   url "https://github.com/MahanRahmati/translate/releases/download/v#{version}/arna_translate_macOS.zip"
   name "Arna"

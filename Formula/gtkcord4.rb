e669c8bd46766ffe8581665ca065273cf9416dbc
Hybras <24651269+hybras@users.noreply.github.com>
2023-10-25 10:35:39 -0400

    gtkcord4 0.0.13 (#108)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gtkcord4.rb
+++ Formula/gtkcord4.rb
@@ -1,8 +1,8 @@
 class Gtkcord4 < Formula
   desc "Gtk4 Discord client"
   homepage "https://github.com/diamondburned/gtkcord4"
-  url "https://github.com/diamondburned/gtkcord4/archive/refs/tags/v0.0.12.tar.gz"
-  sha256 "eecd4cf4ce7848e94b4c52d6fda4f681f0448624e49b76db02ad18110a4602c3"
+  url "https://github.com/diamondburned/gtkcord4/archive/refs/tags/v0.0.13.tar.gz"
+  sha256 "3697c0900e72e60421d75283ca87e5703603eaeaac9d064adaa5dcd09c0c944d"
   license "GPL-3.0-or-later"
 
   depends_on "go" => :build

cb91a3e837103e73dc1a825e2b821f6e269a3d39
hybras <24651269+hybras@users.noreply.github.com>
2023-12-16 10:54:11 -0500

    Update gtkcord4 formula status

--- Formula/gtkcord4.rb
+++ Formula/gtkcord4.rb
@@ -5,6 +5,10 @@ class Gtkcord4 < Formula
   sha256 "d856bcfe4a8e80957f2626fb7068aa83e4af18255c629ee164d1b01e6cbf2147"
   license "GPL-3.0-or-later"
 
+  # This formula doesn't currently build, due to some issues with PKG_CONFIG_PATH.
+  # Adding (almost) every transitive depedency worked at one point, but I got tired of maintaining the list
+  disable! date: "2023-09-14", because: :does_not_build
+
   depends_on "go" => :build
   depends_on "pkg-config" => :build
   depends_on "cairo"

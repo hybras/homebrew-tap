7cc62d9ef94e2c9c90f9829d6561cdbc65d2f72d
Hybras <24651269+hybras@users.noreply.github.com>
2023-12-27 19:48:22 +0000

    webcord 4.6.0

--- Casks/webcord.rb
+++ Casks/webcord.rb
@@ -1,13 +1,13 @@
 cask "webcord" do
-  version "4.5.1"
+  version "4.6.0"
 
   on_arm do
-    sha256 "d30e934ab343ce4cef34b2cc2ec2160093fef11d5c9fcc5ece20a0a5ff50b2f3"
+    sha256 "8d4972983bfd8c14e5433759dfb29c37c9fafd0ccc3748103b140c710c0f8440"
 
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "2802cf5a757aa94d20727c2e259acfa072c54325ca6a80489c052975b0f80783"
+    sha256 "a67217aacd0bac4d256c669e1bac5ee01008780fd82d8146b84b1251b120ff32"
 
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-x64.dmg"
   end

96150d14f9d184bf224eb6d9ff26b0a5ecd0558f
hybras <24651269+hybras@users.noreply.github.com>
2023-12-27 20:54:11 -0500

    Styling

--- Casks/barbox.rb
+++ Casks/barbox.rb
@@ -3,9 +3,9 @@ cask "barbox" do
   sha256 "c563cf1b84b267230e75539373609410cb8d059cfd0789e1a6926e2eb4bc6519"
 
   url "https://github.com/MuhammedKpln/barbox/releases/download/#{version}/BarBox.dmg",
-    verified: "github.com/MuhammedKpln/barbox"
+      verified: "github.com/MuhammedKpln/barbox/"
   name "BarBox"
-  desc "Access disposable emails from the Mac menu bar"
+  desc "Access disposable emails from the menu bar"
   homepage "https://muhammedkpln.github.io/barbox/"
 
   depends_on macos: ">= :mojave"

f0455c26aa4ec0ad7cff722d9c5922a4eea5af3a
hybras <24651269+hybras@users.noreply.github.com>
2023-12-29 22:24:32 -0500

    Amberol v0.10.3

--- Formula/amberol.rb
+++ Formula/amberol.rb
@@ -1,8 +1,8 @@
 class Amberol < Formula
   desc "Music Player"
   homepage "https://apps.gnome.org/app/io.bassi.Amberol/"
-  url "https://gitlab.gnome.org/World/amberol/-/archive/0.9.1/amberol-0.9.1.tar.gz"
-  sha256 "543c96265c382c69708f66e8ab48038c6592d525bc6c7ca6c0fe0458b07bb99e"
+  url "https://gitlab.gnome.org/World/amberol/-/archive/0.10.3/amberol-0.10.3.tar.gz"
+  sha256 "52372ec6f5ba066409e8dfc4a62fdb2c57e79f83a809cf295ff0e040eebb233b"
   license "GPL-3.0-or-later"
 
   depends_on "meson" => :build

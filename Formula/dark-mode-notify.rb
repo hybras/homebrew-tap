645bae37c5c84cd47a20e7d19d3203964ee64103
hybras <24651269+hybras@users.noreply.github.com>
2024-04-12 12:09:30 -0400

    Dark-mode-notify

---
+++ Formula/dark-mode-notify.rb
@@ -0,0 +1,28 @@
+class DarkModeNotify < Formula
+  version "0.1.0"
+  license "MIT"
+  head "https://github.com/bouk/dark-mode-notify.git", branch: "main"
+
+  uses_from_macos "swift" => [:build]
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/#{name}"
+  end
+
+  def post_install
+    etc.mkpath
+    touch etc/"onInterfaceThemeChange"
+  end
+
+  test do
+    system "false"
+  end
+
+  service do
+    run [bin/"dark-mode-notify", etc/"onInterfaceThemeChange"]
+    keep_alive true
+    log_path var/"log/dark-mode-notify.log"
+    error_log_path var/"log/dark-mode-notify.log"
+  end
+end

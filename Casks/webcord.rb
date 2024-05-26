d505e37ebb4e32d9a7ea61d9ad8eda4cdcd902f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-05-26 04:28:43 +0000

    webcord 4.9.2

--- Casks/webcord.rb
+++ Casks/webcord.rb
@@ -1,13 +1,13 @@
 cask "webcord" do
-  version "4.9.1"
+  version "4.9.2"
 
   on_arm do
-    sha256 "a6733e5789564c8ad76e69bc998c9f2143b0a95c25c5fc4cb17b2ebba19b9a95"
+    sha256 "fa07d664b707917217f98de1667fb60b794d4a57021ae4bfaf84682fcd031c52"
 
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "52bc878366f37dcfb2c94b61d711ef6b8441bf49d3a595fd5814c7da6c9e83b3"
+    sha256 "1312dfacf3378cec1055be62dddfd248ba5f171a1e50cdd8b54cda94ee234a0e"
 
     url "https://github.com/SpacingBat3/WebCord/releases/download/v#{version}/WebCord-#{version}-x64.dmg"
   end

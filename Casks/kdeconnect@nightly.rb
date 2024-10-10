500fdd53eeab01625945c3c6becc7b7c9cc5637d
Hybras <24651269+hybras@users.noreply.github.com>
2024-10-10 03:23:59 +0000

    kdeconnect@nightly 4382

--- Casks/kdeconnect@nightly.rb
+++ Casks/kdeconnect@nightly.rb
@@ -1,9 +1,9 @@
 cask "kdeconnect@nightly" do
   arch intel: "x86_64", arm: "arm64"
 
-  version "4330"
-  sha256 intel: "76a686f0a058af015c01b2e95b51e9a4bf48c0cf86ce0283be531b1b51aede1d",
-         arm:   "7c7b94476e683e0f0c67d44730a455eacd43da65924c1b9c65ee050083694839"
+  version "4382"
+  sha256 intel: "d30504e4fbc346166fd27400a9fecc978ca3900bb27c08cdbfc50d2c0cd5b428",
+         arm:   "3909e36fff0d752243aa4e2a45f32542f893738c7f0cea2e67dca899a3532a73"
 
   url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
   name "KDE Connect"

05d1f54fbe571c451afd59e2e3fdf79808c0611c
hybras <24651269+hybras@users.noreply.github.com>
2024-12-10 11:51:56 -0500

    Restore barbox verified stanza

--- Casks/barbox.rb
+++ Casks/barbox.rb
@@ -2,7 +2,8 @@ cask "barbox" do
   version "2.3.0"
   sha256 "c563cf1b84b267230e75539373609410cb8d059cfd0789e1a6926e2eb4bc6519"
 
-  url "https://github.com/MuhammedKpln/barbox/releases/download/#{version}/BarBox.dmg"
+  url "https://github.com/MuhammedKpln/barbox/releases/download/#{version}/BarBox.dmg",
+      verified: "github.com/MuhammedKpln/barbox/"
   name "BarBox"
   desc "Access disposable emails from the menu bar"
   homepage "https://muhammedkpln.github.io/barbox/"

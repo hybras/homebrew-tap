cce46bbfa5e80252946c6883d26ba72b3b63038a
hybras <24651269+hybras@users.noreply.github.com>
2025-05-17 09:16:00 -0400

    Cask voxa

---
+++ Casks/voxa.rb
@@ -0,0 +1,16 @@
+cask "voxa" do
+  version "0.6.8"
+  sha256 "310213d106ca9406b588d79572a18f897ee39196f52b736e6a17cf9c99ef91c1"
+
+  url "https://github.com/voxa-org/Voxa/releases/download/v#{version}/Voxa.#{version}.dmg"
+  name "Voxa"
+  desc "native macOS Discord client"
+  homepage "https://voxa.peril.lol/"
+
+  # depends_on macos: ""
+
+  app "Voxa.app"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: ""
+end

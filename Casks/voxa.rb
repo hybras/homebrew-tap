179d553cc08b7414d61271d464ef8772cb6f2713
hybras <24651269+hybras@users.noreply.github.com>
2025-05-23 16:05:33 -0400

    brew style

--- Casks/voxa.rb
+++ Casks/voxa.rb
@@ -3,7 +3,7 @@ cask "voxa" do
   sha256 "310213d106ca9406b588d79572a18f897ee39196f52b736e6a17cf9c99ef91c1"
 
   url "https://github.com/voxa-org/Voxa/releases/download/v#{version}/Voxa.#{version}.dmg",
-    verified: "github.com/voxa-org"
+      verified: "github.com/voxa-org/"
   name "Voxa"
   desc "Discord client"
   homepage "https://voxa.peril.lol/"

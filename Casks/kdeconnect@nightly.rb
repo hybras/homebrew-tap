12a7e0317808e47d8cef004c53ec89b0f1243bc9
hybras <24651269+hybras@users.noreply.github.com>
2025-06-07 11:17:14 -0400

    use on_macos

--- Casks/kdeconnect@nightly.rb
+++ Casks/kdeconnect@nightly.rb
@@ -1,39 +1,39 @@
 cask "kdeconnect@nightly" do
-  arch intel: "x86_64", arm: "arm64"
+  on_macos do
+    arch intel: "x86_64", arm: "arm64"
 
-  version "5081"
-  sha256 intel:        "0e6d038f1fab5ff8d529e55f75b3d019a41a45f56b4c46fae40aaa512d9d229b",
-         arm:          "f49e5c103a5592d6038afa6293c8a5063d64b2162ca08d2120df6d863c48a086",
-         arm64_linux:  "0",
-         x86_64_linux: "0"
+    version "5081"
+    sha256 intel:        "0e6d038f1fab5ff8d529e55f75b3d019a41a45f56b4c46fae40aaa512d9d229b",
+          arm:          "f49e5c103a5592d6038afa6293c8a5063d64b2162ca08d2120df6d863c48a086"
 
-  url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
-  name "KDE Connect"
-  desc "Enabling communication between all your devices"
-  homepage "https://kdeconnect.kde.org/"
+    url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
+    name "KDE Connect"
+    desc "Enabling communication between all your devices"
+    homepage "https://kdeconnect.kde.org/"
 
-  livecheck do
-    url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-x86_64/"
-    regex(/href=.*?kdeconnect-kde-master-(\d+)-macos-clang-x86_64\.dmg/i)
+    livecheck do
+      url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-x86_64/"
+      regex(/href=.*?kdeconnect-kde-master-(\d+)-macos-clang-x86_64\.dmg/i)
+    end
+
+    depends_on formula: "dbus"
+
+    app "KDE Connect.app"
+    binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-cli"
+    binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-sms"
+
+    uninstall quit: "org.kde.kdeconnect"
+
+    zap trash: [
+      "~/Library/Application Support/kpeoplevcard",
+      "~/Library/Caches/kdeconnect.sms",
+      "~/Library/Preferences/kdeconnect",
+      "~/Library/Preferences/kdeconnect_runcommand",
+      "~/Library/Preferences/kdeconnect_sendnotifications",
+      "~/Library/Preferences/kdeconnect_share",
+      "~/Library/Preferences/org.kde.kdeconnect.plist",
+    ]
+
+    caveats "Installation may fail due to nightly build artifacts expiring"
   end
-
-  depends_on formula: "dbus"
-
-  app "KDE Connect.app"
-  binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-cli"
-  binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-sms"
-
-  uninstall quit: "org.kde.kdeconnect"
-
-  zap trash: [
-    "~/Library/Application Support/kpeoplevcard",
-    "~/Library/Caches/kdeconnect.sms",
-    "~/Library/Preferences/kdeconnect",
-    "~/Library/Preferences/kdeconnect_runcommand",
-    "~/Library/Preferences/kdeconnect_sendnotifications",
-    "~/Library/Preferences/kdeconnect_share",
-    "~/Library/Preferences/org.kde.kdeconnect.plist",
-  ]
-
-  caveats "Installation may fail due to nightly build artifacts expiring"
 end

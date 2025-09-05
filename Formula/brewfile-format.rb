341d28f3b67612f6793c664af53366d822d26590
hybras <24651269+hybras@users.noreply.github.com>
2025-09-04 22:31:50 -0400

    Testcase for brewfile-format

--- Formula/brewfile-format.rb
+++ Formula/brewfile-format.rb
@@ -13,15 +13,19 @@ class BrewfileFormat < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test brewfile-format`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    # Create a sample Brewfile
+    (testpath/"Brewfile").write <<~EOS
+      cask "google-chrome"
+      brew "wget"
+    EOS
+
+    # Run the formatter and check output
+    output = shell_output("#{bin}/brewfile-format #{testpath}/Brewfile")
+    expected = <<~EOS
+      brew "wget"
+
+      cask "google-chrome"
+    EOS
+    assert_equal expected, output
   end
 end

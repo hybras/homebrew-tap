96337dd20c3ea63faa43300b613e42981aae0ebd
Hybras <24651269+hybras@users.noreply.github.com>
2025-09-05 03:43:43 +0000

    brewfile-format: add 0.2.0 bottle.

--- Formula/brewfile-format.rb
+++ Formula/brewfile-format.rb
@@ -6,6 +6,14 @@ class BrewfileFormat < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://gitlab.com/hybras/brewfile-format.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/hybras/tap"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45081e1a54ee8537dd4334d7ea0cf1911ed155b05774f9c7836865e235bb6759"
+    sha256 cellar: :any_skip_relocation, ventura:       "218d40875bd933ee1f0a4108870a66b8aba422af9c27e90c8aefd48a4b2e7467"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "90fcc0db56fb9ac1a9c92a93a14b2e3d8b969360aea52fea5035bd9ee9450ffd"
+  end
+
   depends_on "rust" => :build
 
   def install

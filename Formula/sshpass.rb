500917f7a0e3c6e92ef085eb211e4b473350d7e3
Hybras <24651269+hybras@users.noreply.github.com>
2025-09-17 01:29:14 +0000

    sshpass: add 1.10 bottle.

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -5,6 +5,14 @@ class Sshpass < Formula
   sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
   license "GPL-2.0-only"
 
+  bottle do
+    root_url "https://ghcr.io/v2/hybras/tap"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "84ba8dc042ab4161ab7270f1227402635ae3438a25aeb6fcf9505e4251bf254c"
+    sha256 cellar: :any_skip_relocation, ventura:       "d8910d3b8d47c192efdb060e0bbecd647372dedfc770304e2fcf6693ea510b07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "889ca896637a79fe06f21fb84a71e1072c1177c7ce0315c20c476d88cdea0ce2"
+  end
+
   def install
     system "./configure",
            *std_configure_args,

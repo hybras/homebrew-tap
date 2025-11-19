42f9efbc962934183726718bdc345a9e425da154
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-19 07:29:34 +0000

    kdeconnect@nightly 5535

--- Casks/kdeconnect@nightly.rb
+++ Casks/kdeconnect@nightly.rb
@@ -2,9 +2,9 @@ cask "kdeconnect@nightly" do
   on_macos do
     arch intel: "x86_64", arm: "arm64"
 
-    version "5475"
-    sha256 intel: "99f2e4d268ad58da2136e725b73d5c0dfb826eb3dee214e5dc466f09d471938d",
-           arm:   "08bac7d5e9feff41b7714ddbabce88294ee86bed22097cbf5bb783b9e47d1c9d"
+    version "5535"
+    sha256 intel: "dd72952cfc3ae5251a91d317b0959678a1687f30332644645ae248261a47035a",
+           arm:   "b7593c1664703942f49048f23e7aa5bceeafd84480be9fbc327654a92f791da1"
 
     url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
     name "KDE Connect"

9c148f07d8f7fe4ddd3f111ee3b5f2a4ee43108b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-13 09:51:53 +0000

    kdeconnect@nightly 6191

--- Casks/kdeconnect@nightly.rb
+++ Casks/kdeconnect@nightly.rb
@@ -2,9 +2,9 @@ cask "kdeconnect@nightly" do
   on_macos do
     arch intel: "x86_64", arm: "arm64"
 
-    version "6130"
-    sha256 intel: "a1a5c327eb85ac87cff54e632699b9c7055ee8640a077bc0fd04bf1d2c0e2e8d",
-           arm:   "69594696977740a78245bead0644ed631e11dbea4c731984addc8bc0c8cdd505"
+    version "6191"
+    sha256 intel: "2a75185579776a6e1feb7bac2fabcca2251481b5ea84f8be542ec6fa62c204c5",
+           arm:   "237293209176c68f3b5e4462686bcc77e7172958e8b2c326d281c2e4c987dc7b"
 
     url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
     name "KDE Connect"
@@ -16,7 +16,7 @@ cask "kdeconnect@nightly" do
       regex(/href=.*?kdeconnect-kde-master-(\d+)-macos-clang-x86_64\.dmg/i)
     end
 
-    depends_on macos: ">= :ventura"
+    depends_on macos: :ventura
     depends_on formula: "dbus"
 
     app "KDE Connect.app"
